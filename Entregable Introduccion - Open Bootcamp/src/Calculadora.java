c2abbfe319e8c9ea10ca70a796a767a628365aed
Isabel Restrepo <mariaarpo8@gmail.com>
2022-10-03 15:57:10 -0500

    Entregable Introducción a la programación

---
+++ Entregable Introduccion - Open Bootcamp/src/Calculadora.java
@@ -0,0 +1,9 @@
+public class Calculadora {
+    public static void main(String[] args) {
+        System.out.println(numeros(7,3,2));
+
+    }
+    public static int numeros(int num1,int num2,int num3){
+        return(num1+num2+num3);
+    }
+}
